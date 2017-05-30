3e7316145b7828d9bfa96dd9233a2bf0fb86748f
MizoTake <mizotake123@gmail.com>
2017-05-31 00:05:03 +0900

    first

---
+++ Assets/FingerAuth/Scripts/FingerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FingerAuth {
+
+	public class FingerManager : MonoBehaviour {
+
+		private const string JAVA_CLASS_NAME = "com.example.fingerauthunity.lib.FingerAuth";
+		//Androidプラグインからコールバックを受け取るため
+		private const string OBJECT_NAME = "FingerManager"; 
+
+		private FingerAuthState _state;
+
+		public FingerAuthState State { get { return _state; } }
+
+		// Use this for initialization
+		void Start () {
+			_state = FingerAuthState.INIT;
+			gameObject.name = OBJECT_NAME;
+			FingerAuthStart();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		public void FingerAuthStart() {
+			using (AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer")) {
+				using (AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity")) {
+					using (AndroidJavaClass plugin = new AndroidJavaClass(JAVA_CLASS_NAME)) {
+						plugin.CallStatic("AuthStart", activity);
+					}
+				}
+			}
+		}
+
+		//CallBackで処理結果が返ってくる
+		public void FingerAuthResult(string message) {
+			_state = (FingerAuthState)int.Parse(message);
+		}
+	}
+
+
+public enum FingerAuthState {
+	INIT = -1,
+	WAIT = 0,
+	SUCCESS = 1,
+	FAILD = 2
+}
+
+}
---
+++ Assets/ManagerResultText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FingerAuth;
+
+public class ManagerResultText : MonoBehaviour {
+
+	[SerializeField]
+	private FingerManager _finger;
+
+	private Text _text;
+
+	// Use this for initialization
+	void Start () {
+		_text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_text.text = FingerStateString(_finger.State);
+	}
+
+	private string FingerStateString(FingerAuthState state) {
+		switch(state) {
+			case FingerAuthState.INIT:
+				return "初期化状態";
+			case FingerAuthState.WAIT:
+				return "指紋認証待ち状態";
+			case FingerAuthState.SUCCESS:
+				return "指紋認証成功";
+			case FingerAuthState.FAILD:
+				return "指紋認証失敗";
+		}
+		return "";
+	}
+}
